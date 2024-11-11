ba7edffd8b51c3b17f9a6b6a78830989ff5128f3
Palash Dash <palashdash804@gmail.com>
2024-11-11 20:22:03 +0600

    dm

--- 2. Dictionary_Adding_Key-Value_Pairs.py
+++ 2. Dictionary_Adding_Key-Value_Pairs.py
@@ -1,10 +1,6 @@
-a = {'place': 'Dhaka', 'postal_code': 1219, 'country': 'Bangladesh'}      
 
-place = a['place']                                         
-postal_code = a.get('postal_code')
-country = a['country']
+a = {'name': 'John', 'age': 25}
 
+a['city'] = 'New York'
 
-print('place:', place)                                          
-print('postal code:', postal_code)
-print('country:', country)
+print('Updated Dictionary:', a)
